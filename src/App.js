75e22f81e1de845ab02dee89feab92ba82e056ac
Birhat03 <birhat2003@hotmail.com>
2023-03-11 05:02:19 +0100

    fixes

--- src/App.js
+++ src/App.js
@@ -9,15 +9,19 @@ function App() {
   const [movies, setMovies] = useState([])
   const [search, setSearch] = useState('James Bond')
 
+  
   const getMovies = async() =>{
+    
     const response = await fetch(`http://www.omdbapi.com/?s=${search}&type=movie&apikey=689377f1`)
     const data = await response.json()
     console.log(data.Search)
     setMovies(data.Search)
     
+    
     const response2 = await fetch(`http://www.omdbapi.com/?t=${search}&type=movie&apikey=689377f1`)
     const data2 = await response2.json()
     console.log(data2)
+    //setMovies(data2)
     
   }
   useEffect(() =>{
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,17 @@
 import MovieCard from './MovieCard';
 import Search from './Search';
 export default function Main({movies, setSearch, getMovies}){
+    //https://dev.to/arikaturika/use-this-trick-to-map-over-single-objects-in-javascript-38nb
+    if (!Array.isArray(movies)){
+        movies = [movies];
+    }
     return (
       <>
       <Search setSearch={setSearch} getMovies={getMovies} />
       <section className='movie-view'>
         <h2>Filmer basert på ditt søk:</h2>
           {movies.map((movie, index) => (
-                  <MovieCard key={index} title={movie?.Title} img={movie.Poster} year={movie.Year} id={movie.imdbID}/> 
+                  <MovieCard key={index} title={movie.Title} img={movie.Poster} year={movie.Year} id={movie.imdbID}/> 
              
           ))}
         </section>
