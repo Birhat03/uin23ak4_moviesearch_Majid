0b32e1d5bffe605bd7fa815432e9f62b819789f3
Birhat03 <birhat2003@hotmail.com>
2023-03-14 22:41:25 +0100

    fikset litt på html og css

--- src/components/MoviePage.js
+++ src/components/MoviePage.js
@@ -14,12 +14,14 @@ export default function MoviePage({movies}){
      e.target.src = 'https://place-hold.it/300x500&text=(FilmPoster)&fontsize=16' // kode hentet fra: https://stackoverflow.com/questions/36418806/react-use-img-onerror
   }}/>
                 <p className="plot">{movie[9]}</p>
-                <p>Utgivelsesdato: <strong>{movie[3]}</strong></p>
-                <p>Språk: <strong>{movie[10]}</strong></p>
-                <p>Land: <strong>{movie[11]}</strong></p>
-                <p>Regissør: <strong> {movie[6]}</strong></p>
-                <p>Forfattere: <strong>{movie[7]}</strong></p>
-                <p>Skuespillere: <strong>{movie[8]}</strong></p>
+            <ul className="info">
+                <li>Utgivelsesdato: <strong>{movie[3]}</strong></li>
+                <li>Språk: <strong>{movie[10]}</strong></li>
+                <li>Land: <strong>{movie[11]}</strong></li>
+                <li>Regissør: <strong> {movie[6]}</strong></li>
+                <li>Forfattere: <strong>{movie[7]}</strong></li>
+                <li>Skuespillere: <strong>{movie[8]}</strong></li>
+            </ul>
             </article>
         </section>
     )
